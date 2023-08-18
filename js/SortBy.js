10ceab0a7e16211fc16d4a8478533e919f51369b
calle <ozmehak.calle@gmail.com>
2023-08-18 04:19:46 +0200

    Sort by in js

---
+++ js/SortBy.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    
+            return sortedArray = arr.sort((a, b) => fn(a) - fn(b))
+           
+    
+
+
+};
